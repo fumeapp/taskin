b2a539589083d30d84917dae4f86d831e49fabab
kevin olson <acidjazz@gmail.com>
2024-05-04 22:31:11 -0500

    🎨 readme updates

--- examples/progress/main.go
+++ examples/progress/main.go
@@ -10,34 +10,11 @@ func main() {
 
 	tasks := taskin.New(taskin.Tasks{
 		{
-			Title: "Task 1",
-			// sleep for 3 seconds then return nil
-			Task: func(t *taskin.Task) error {
-				for i := 0; i < 2; i++ {
-					t.Title = fmt.Sprintf("Task 1 - [%d/%d]", i+1, 2)
-					time.Sleep(1 * time.Second)
-				}
-				return nil
-			},
-		},
-		{
-			Title: "Task 2 Progress",
-			// sleep for 3 seconds then return nil
+			Title: "Progress Task",
 			Task: func(t *taskin.Task) error {
 				for i := 0; i < 5; i++ {
 					t.Progress(i+1, 5)
 					t.Title = fmt.Sprintf("Task 2 - [%d/%d]", i+1, 5)
-					time.Sleep(1 * time.Second)
-				}
-				return nil
-			},
-		},
-		{
-			Title: "Task 3",
-			// sleep for 3 seconds then return nil
-			Task: func(t *taskin.Task) error {
-				for i := 0; i < 2; i++ {
-					t.Title = fmt.Sprintf("Task 3 - [%d/%d]", i+1, 2)
 					time.Sleep(1 * time.Second)
 				}
 				return nil
