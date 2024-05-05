52d714d1721d91a5405aed7809980337aac80fc9
kevin olson <acidjazz@gmail.com>
2024-05-04 22:33:56 -0500

    🎨 fix simple in readme

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -25,7 +25,7 @@ func main() {
 			// sleep for 3 seconds then return nil
 			Task: func(t *taskin.Task) error {
 				for i := 0; i < 3; i++ {
-					t.Title = fmt.Sprintf("Task 1: [%d/3] seconds have passed", i+1)
+					t.Title = fmt.Sprintf("Task 2: [%d/3] seconds have passed", i+1)
 					time.Sleep(1 * time.Second)
 				}
 				return fmt.Errorf("task 2 failed")
